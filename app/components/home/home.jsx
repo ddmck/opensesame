18ff0cc6df820fbb60f1ecb4560a5cf1539e77dc
Donald McKendrick <ddmckendrick@gmail.com>
2016-02-22 02:09:46 -0500

    add a link to lists from the jumbotron

--- app/components/home/home.jsx
+++ app/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default React.createClass({
   render: function () {
@@ -7,7 +8,7 @@ export default React.createClass({
         <div className="container">
           <h1>Welcome to How I Learned!</h1>
           <p>We are creating a community of people who share the process for learning new things.</p>
-          <p><a className="btn btn-primary btn-lg" href="https://medium.com/@petergault/how-i-learned-vision-statement-ded505ba74ac#.3dj3vuv6v" target="_blank" role="button">Learn more</a></p>
+          <p><Link className="btn btn-primary btn-lg" to={"/lists/"}>Featured Lists</Link> <a className="btn btn-primary btn-lg" href="https://medium.com/@petergault/how-i-learned-vision-statement-ded505ba74ac#.3dj3vuv6v" target="_blank" role="button">Learn more</a></p>
         </div>
       </div>
     )
